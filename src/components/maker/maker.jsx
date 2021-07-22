c5f259a5bcc360db2982faf4467288e3fc28f1a8
Gyuo <gosim8611@gmail.com>
2021-07-22 18:42:45 +0900

    remove warnings firebase clean up

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -23,7 +23,7 @@ const Maker = ({ FileInput, authService, cardRepository }) => {
       setCards(cards);
     });
     return () => stopSync();
-  }, [userId]);
+  }, [userId, cardRepository]);
 
   useEffect(() => {
     authService.onAuthChange((user) => {
@@ -33,7 +33,7 @@ const Maker = ({ FileInput, authService, cardRepository }) => {
         history.push("/");
       }
     });
-  });
+  }, [userId, authService, history]);
 
   const createOrUpdateCard = (card) => {
     setCards((cards) => {
